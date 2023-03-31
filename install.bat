f5112b0f0dcc250891553e7a66a2c66603bba1ee
ExodusLogger <129537622+VelocityLogger@users.noreply.github.com>
2023-03-31 15:00:24 -0700

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,12 @@
+pip3 install psutil
+pip3 install shutil
+pip3 install requests
+pip3 install crypto
+pip3 install platform
+pip3 install sqlite3
+pip3 install win32crypt
+pip3 install nuitka
+pip3 install pyinstaller
+pip3 install pywin32
+pip3 install pyqt5
+pip3 install cryptography
